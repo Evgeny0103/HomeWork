bea5063ed4b943872b30298f02e245609848c150
Evgeny0103 <38032854+Evgeny0103@users.noreply.github.com>
2021-01-20 11:58:34 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConsoleColor prevColor = Console.BackgroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Green;
+                DateTime curDate = DateTime.Today;
+                Console.WriteLine("Введите ваше имя ");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Привет {name} сегодня {curDate}");
+                Console.Read();
+            }
+        }
+    }
+
+
